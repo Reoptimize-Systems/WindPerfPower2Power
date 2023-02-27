1afed533410b61fc8fe523e3a7a74dc317a9ed6a
reos-rcrozier <91322120+reos-rcrozier@users.noreply.github.com>
2023-02-27 16:24:18 +0000

    findWakeFreeConditions.m: fix indentation (from editing in octave instead of matlab)
    indentation of functions was 2 spaces instead of 4

--- findWakeFreeConditions.m
+++ findWakeFreeConditions.m
@@ -68,47 +68,47 @@ function a = findWakeFreeConditions(Data, varargin)
 %   See the License for the specific language governing permissions and
 %   limitations under the License.
 
-  % Default Parameters
-  options.MinimumPower = 10;
-  options.WindDirectionBinSize = 10;
-
-  options = parse_pv_pairs (options, varargin);
-
-  %% Check Validity of optional parameters
-  if ~(isreal(options.MinimumPower) && isscalar(options.MinimumPower))
+    % Default Parameters
+    options.MinimumPower = 10;
+    options.WindDirectionBinSize = 10;
+    
+    options = parse_pv_pairs (options, varargin);
+    
+    %% Check Validity of optional parameters
+    if ~(isreal(options.MinimumPower) && isscalar(options.MinimumPower))
       error('Expected MinimumPower to be a real numeric scalar');
-  end
-
-  if ~(isreal(options.WindDirectionBinSize) && isscalar(options.WindDirectionBinSize))
+    end
+    
+    if ~(isreal(options.WindDirectionBinSize) && isscalar(options.WindDirectionBinSize))
       error('Expected WindDirectionBinSize to be a real numeric scalar');
-  end
-
-  % Check Validity of Data structure members
-  if ~isfield(Data,'ControlWTPre') ; error('Expected to find ControlWTPre in Data structure'); end
-  if ~isfield(Data,'ControlWTPost') ; error('Expected to find ControlWTPost in Data structure'); end
-  if ~isfield(Data,'TestWTPre') ; error('Expected to find TestWTPre in Data structure'); end
-  if ~isfield(Data,'TestWTPost') ; error('Expected to find TestWTPost in Data structure'); end
-
-  TimeExists = structfun(@(x) isfield(x,'Time'), Data);
-  PowerActiveExists = structfun(@(x) isfield(x,'PowerActive'), Data);
-  if ~all(TimeExists); error('Expected to find "Time" field'); end
-  if ~all(PowerActiveExists); error('Expected to find "PowerActive" field'); end
-
-
-  %%%%%%%%%%%%%%%%%%%%%%%%% Start of function %%%%%%%%%%%%%%%%%%%%%%%%%
-
-  %% Remove data where wind turbine is not on grid
-  [TimePre, iPreCtrl, iPreMod] = intersect( Data.ControlWTPre.Time, ...
-                                            Data.TestWTPre.Time, ...
-                                            'stable' );
-
-  [TimePost, iPostCtrl, iPostMod] = intersect( Data.ControlWTPost.Time, ...
-                                               Data.TestWTPost.Time, ...
-                                               'stable' );
-
-  WindDirectionBins = linspace(0, 360, 720 / (options.WindDirectionBinSize * 2) + 1);
-
-  % Bins = discretize(WindDirectionData,WindDirectionBins);
+    end
+    
+    % Check Validity of Data structure members
+    if ~isfield(Data,'ControlWTPre') ; error('Expected to find ControlWTPre in Data structure'); end
+    if ~isfield(Data,'ControlWTPost') ; error('Expected to find ControlWTPost in Data structure'); end
+    if ~isfield(Data,'TestWTPre') ; error('Expected to find TestWTPre in Data structure'); end
+    if ~isfield(Data,'TestWTPost') ; error('Expected to find TestWTPost in Data structure'); end
+    
+    TimeExists = structfun(@(x) isfield(x,'Time'), Data);
+    PowerActiveExists = structfun(@(x) isfield(x,'PowerActive'), Data);
+    if ~all(TimeExists); error('Expected to find "Time" field'); end
+    if ~all(PowerActiveExists); error('Expected to find "PowerActive" field'); end
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%% Start of function %%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %% Remove data where wind turbine is not on grid
+    [TimePre, iPreCtrl, iPreMod] = intersect( Data.ControlWTPre.Time, ...
+                                              Data.TestWTPre.Time, ...
+                                              'stable' );
+    
+    [TimePost, iPostCtrl, iPostMod] = intersect( Data.ControlWTPost.Time, ...
+                                                 Data.TestWTPost.Time, ...
+                                                 'stable' );
+    
+    WindDirectionBins = linspace(0, 360, 720 / (options.WindDirectionBinSize * 2) + 1);
+    
+    % Bins = discretize(WindDirectionData,WindDirectionBins);
 
 
 end
