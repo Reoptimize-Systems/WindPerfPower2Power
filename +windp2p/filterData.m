b16aa6acd660199d3e66d5d4da26f2b0e60c9fa6
reos-rcrozier <91322120+reos-rcrozier@users.noreply.github.com>
2023-03-14 00:38:36 +0000

    Continued development of data filtering code, added test script

--- +windp2p/filterData.m
+++ +windp2p/filterData.m
@@ -43,18 +43,24 @@ function filtered_data = filterData (data, varargin)
     options.OperationalRPMThreshold = 4;
     options.OperationalWindSpeedThreshold = [];
     options.OperationalFilterMethod = 'rpm';
-    options.CurtailedFilterMethod = 'flag';
+    options.CurtailedFilterMethod = 'none';
     options.CurtailedDailyTimesAndDurations = [];
-    options.WindDirectionThreshNBins = 12;
+    options.WindDirectionThreshNBins = 37;
     options.WindDirectionNumSamplesThresh = 1000;
     options.PowerRangeThreshBinSize = 5000;
     options.PowerRangeNumSamplesThresh = 1000;
+    options.DataGapInfillThreshold = 5;
+    options.SyncronisedTimeStepSize = 1;
+    options.TestTurbineTimeShift = 0;
 
 
     options = windp2p.parse_pv_pairs (options, varargin);
 
     % TODO: need input validation here
 
+    % syncronize and interpolate the data time series
+    filtered_data = syncronize_data (data, 'ControlWTPre', 'TestWTPre', options);
+    filtered_data = syncronize_data (filtered_data, 'ControlWTPost', 'TestWTPost', options);
 
     % filter by Test Turbine and Reference Turbine operational, filtering by
     % status code or e.g. by RPM signal
@@ -62,17 +68,21 @@ function filtered_data = filterData (data, varargin)
 
         case 'rpm'
 
-            good_inds = find(filtered_data.ControlWTPre.RPM >= options.OperationalRPMThreshold);
-            filtered_data.ControlWTPre = leave_data_good_inds_all_fields (filtered_data.ControlWTPre, good_inds);
+            bad_inds = find(filtered_data.ControlWTPre.RPM < options.OperationalRPMThreshold);
+            filtered_data.ControlWTPre =  strip_data_bad_inds_all_fields (filtered_data.ControlWTPre, bad_inds);
+            filtered_data.TestWTPre = strip_data_bad_inds_all_fields (filtered_data.TestWTPre, bad_inds);
 
-            good_inds = find(filtered_data.ControlWTPost.RPM >= options.OperationalRPMThreshold);
-            filtered_data.ControlWTPost = leave_data_good_inds_all_fields (filtered_data.ControlWTPost, good_inds);
+            bad_inds = find(filtered_data.ControlWTPost.RPM < options.OperationalRPMThreshold);
+            filtered_data.ControlWTPost = strip_data_bad_inds_all_fields (filtered_data.ControlWTPost, bad_inds);
+            filtered_data.TestWTPost = strip_data_bad_inds_all_fields (filtered_data.TestWTPost, bad_inds);
 
-            good_inds = find(filtered_data.TestWTPre.RPM >= options.OperationalRPMThreshold);
-            filtered_data.TestWTPre = leave_data_good_inds_all_fields (filtered_data.TestWTPre, good_inds);
+            bad_inds = find(filtered_data.TestWTPre.RPM < options.OperationalRPMThreshold);
+            filtered_data.TestWTPre = strip_data_bad_inds_all_fields (filtered_data.TestWTPre, bad_inds);
+            filtered_data.ControlWTPre =  strip_data_bad_inds_all_fields (filtered_data.ControlWTPre, bad_inds);
 
-            good_inds = find(filtered_data.TestWTPost.RPM >= options.OperationalRPMThreshold);
-            filtered_data.TestWTPost = leave_data_good_inds_all_fields (filtered_data.TestWTPost, good_inds);
+            bad_inds = find(filtered_data.TestWTPost.RPM < options.OperationalRPMThreshold);
+            filtered_data.TestWTPost = strip_data_bad_inds_all_fields (filtered_data.TestWTPost, bad_inds);
+            filtered_data.ControlWTPost = strip_data_bad_inds_all_fields (filtered_data.ControlWTPost, bad_inds);
 
 
         case 'flag'
@@ -96,10 +106,24 @@ function filtered_data = filterData (data, varargin)
                         options.OperationalFilterMethod);
             end
 
+            % leave only pre data where both turbines are simultaneously
+            % operational
             filtered_data.ControlWTPre = leave_data_good_inds_all_fields (filtered_data.ControlWTPre, data.ControlWTPre.IsOperational);
+            filtered_data.TestWTPre = leave_data_good_inds_all_fields (filtered_data.TestWTPre, data.ControlWTPre.IsOperational);
+
             filtered_data.ControlWTPost = leave_data_good_inds_all_fields (filtered_data.ControlWTPost, data.ControlWTPost.IsOperational);
+            filtered_data.TestWTPost = leave_data_good_inds_all_fields (filtered_data.TestWTPost, data.ControlWTPost.IsOperational);
+
+
             filtered_data.TestWTPre = leave_data_good_inds_all_fields (filtered_data.TestWTPre, data.TestWTPre.IsOperational);
+            filtered_data.ControlWTPre = leave_data_good_inds_all_fields (filtered_data.ControlWTPre, data.TestWTPre.IsOperational);
+
             filtered_data.TestWTPost = leave_data_good_inds_all_fields (filtered_data.TestWTPost, data.TestWTPost.IsOperational);
+            filtered_data.ControlWTPost = leave_data_good_inds_all_fields (filtered_data.ControlWTPost, data.TestWTPost.IsOperational);
+
+        case 'none'
+
+            % do nothing
 
         otherwise
 
@@ -156,9 +180,21 @@ function filtered_data = filterData (data, varargin)
             end
 
             filtered_data.ControlWTPre = strip_data_bad_inds_all_fields (filtered_data.ControlWTPre, data.ControlWTPre.IsCurtailed);
+            filtered_data.TestWTPre = strip_data_bad_inds_all_fields (filtered_data.TestWTPre, data.ControlWTPre.IsCurtailed);
+
             filtered_data.ControlWTPost = strip_data_bad_inds_all_fields (filtered_data.ControlWTPost, data.ControlWTPost.IsCurtailed);
+            filtered_data.TestWTPost = strip_data_bad_inds_all_fields (filtered_data.TestWTPost, data.ControlWTPost.IsCurtailed);
+
             filtered_data.TestWTPre = strip_data_bad_inds_all_fields (filtered_data.TestWTPre, data.TestWTPre.IsCurtailed);
+            filtered_data.ControlWTPre = strip_data_bad_inds_all_fields (filtered_data.ControlWTPre, data.TestWTPre.IsCurtailed);
+
             filtered_data.TestWTPost = strip_data_bad_inds_all_fields (filtered_data.TestWTPost, data.TestWTPost.IsCurtailed);
+            filtered_data.ControlWTPost = strip_data_bad_inds_all_fields (filtered_data.ControlWTPost, data.TestWTPost.IsCurtailed);
+
+
+        case 'none'
+
+            % do nothing
 
         otherwise
 
@@ -169,15 +205,19 @@ function filtered_data = filterData (data, varargin)
     % filter by no blade icing
     good_inds = find(filtered_data.ControlWTPre.TemperatureExternal > options.IcingFilterTemperature);
     filtered_data.ControlWTPre = leave_data_good_inds_all_fields (filtered_data.ControlWTPre, good_inds);
+    filtered_data.TestWTPre = leave_data_good_inds_all_fields (filtered_data.TestWTPre, good_inds);
 
     good_inds = find(filtered_data.ControlWTPost.TemperatureExternal > options.IcingFilterTemperature);
     filtered_data.ControlWTPost = leave_data_good_inds_all_fields (filtered_data.ControlWTPost, good_inds);
+    filtered_data.TestWTPost = leave_data_good_inds_all_fields (filtered_data.TestWTPost, good_inds);
 
     good_inds = find(filtered_data.TestWTPre.TemperatureExternal > options.IcingFilterTemperature);
     filtered_data.TestWTPre = leave_data_good_inds_all_fields (filtered_data.TestWTPre, good_inds);
+    filtered_data.ControlWTPre = leave_data_good_inds_all_fields (filtered_data.ControlWTPre, good_inds);
 
     good_inds = find(filtered_data.TestWTPost.TemperatureExternal > options.IcingFilterTemperature);
     filtered_data.TestWTPost = leave_data_good_inds_all_fields (filtered_data.TestWTPost, good_inds);
+    filtered_data.ControlWTPost = leave_data_good_inds_all_fields (filtered_data.ControlWTPost, good_inds);
 
 
     % optionally filter by Test Turbine and Reference Turbine free of wakes is often
@@ -188,10 +228,11 @@ function filtered_data = filterData (data, varargin)
 
     end
 
-    % In Testing Period: use only wind direction ranges and power ranges also
-    % covered by Training Period
+    % In Testing Period: use only wind direction ranges and power ranges
+    % also covered by Training Period
 
-    % make histogram of nacelle direction ranges for both turbine training (pre) data set
+   WindDirectionNumSamplesThresh % make histogram of nacelle direction ranges for both turbine training
+    % (pre) data sets
     nacelle_direction_edges = linspace(0, 360, options.WindDirectionThreshNBins);
 
     [N_samples_ControlWTPre, nacelle_direction_edges] = ...
@@ -199,9 +240,10 @@ function filtered_data = filterData (data, varargin)
 
     N_samples_TestWTPre = histcounts(filtered_data.TestWTPre.DirectionNacelle, nacelle_direction_edges);
 
-    % find wind directions where number of samples are greater a minimum threshold of samples
-    good_directions = N_samples_ControlWTPre >= options.WindDirectionNumSamplesThresh ...
-                        && N_samples_TestWTPre >= options.WindDirectionNumSamplesThresh
+    % find wind directions where number of samples are greater a minimum
+    % threshold of samples
+    good_directions = N_samples_ControlWDirectionNacelleTPre >= options.WindDirectionNumSamplesThresh ...
+                        & N_samples_TestWTPre >= options.WindDirectionNumSamplesThresh;
 
 
     % keep only data with these wind directions from all four data sets
@@ -210,22 +252,22 @@ function filtered_data = filterData (data, varargin)
         if good_directions(ind) == false
 
             bad_inds = find(filtered_data.ControlWTPre.DirectionNacelle >= edges(ind) ...
-                              && filtered_data.ControlWTPre.DirectionNacelle >= edges(ind+1) );
+                              & filtered_data.ControlWTPre.DirectionNacelle >= edges(ind+1) );
 
             filtered_data.ControlWTPre = strip_data_bad_inds_all_fields (filtered_data.ControlWTPre, bad_inds);
 
             bad_inds = find(filtered_data.ControlWTPost.DirectionNacelle >= edges(ind) ...
-                              && filtered_data.ControlWTPost.DirectionNacelle >= edges(ind+1) );
+                              & filtered_data.ControlWTPost.DirectionNacelle >= edges(ind+1) );
 
             filtered_data.ControlWTPost = strip_data_bad_inds_all_fields (filtered_data.ControlWTPost, bad_inds);
 
             bad_inds = find(filtered_data.TestWTPre.DirectionNacelle >= edges(ind) ...
-                              && filtered_data.TestWTPre.DirectionNacelle >= edges(ind+1) );
+                              & filtered_data.TestWTPre.DirectionNacelle >= edges(ind+1) );
 
             filtered_data.TestWTPre = strip_data_bad_inds_all_fields (filtered_data.TestWTPre, bad_inds);
 
             bad_inds = find(filtered_data.TestWTPost.DirectionNacelle >= edges(ind) ...
-                              && filtered_data.TestWTPost.DirectionNacelle >= edges(ind+1) );
+                              & filtered_data.TestWTPost.DirectionNacelle >= edges(ind+1) );
 
             filtered_data.TestWTPost = strip_data_bad_inds_all_fields (filtered_data.TestWTPost, bad_inds);
 
@@ -264,22 +306,22 @@ function filtered_data = filterData (data, varargin)
         if good_directions(ind) == false
 
             bad_inds = find(filtered_data.ControlWTPre.PowerActive >= edges(ind) ...
-                              && filtered_data.ControlWTPre.PowerActive >= edges(ind+1) );
+                              & filtered_data.ControlWTPre.PowerActive >= edges(ind+1) );
 
             filtered_data.ControlWTPre = strip_data_bad_inds_all_fields (filtered_data.ControlWTPre, bad_inds);
 
             bad_inds = find(filtered_data.ControlWTPost.PowerActive >= edges(ind) ...
-                              && filtered_data.ControlWTPost.PowerActive >= edges(ind+1) );
+                              & filtered_data.ControlWTPost.PowerActive >= edges(ind+1) );
 
             filtered_data.ControlWTPost = strip_data_bad_inds_all_fields (filtered_data.ControlWTPost, bad_inds);
 
             bad_inds = find(filtered_data.TestWTPre.PowerActive >= edges(ind) ...
-                              && filtered_data.TestWTPre.PowerActive >= edges(ind+1) );
+                              & filtered_data.TestWTPre.PowerActive >= edges(ind+1) );
 
             filtered_data.TestWTPre = strip_data_bad_inds_all_fields (filtered_data.TestWTPre, bad_inds);
 
             bad_inds = find(filtered_data.TestWTPost.PowerActive >= edges(ind) ...
-                              && filtered_data.TestWTPost.PowerActive >= edges(ind+1) );
+                              & filtered_data.TestWTPost.PowerActive >= edges(ind+1) );
 
             filtered_data.TestWTPost = strip_data_bad_inds_all_fields (filtered_data.TestWTPost, bad_inds);
 
@@ -292,28 +334,100 @@ function filtered_data = filterData (data, varargin)
 
 end
 
+function data = syncronize_data (data, field1, field2, options)
 
-function data = leave_data_good_inds_all_fields (turbine_data, good_inds)
+    % shift the test turbine time by the specified time in seconds (default
+    % is zero, i.e. no shift). 
+    data.(field2).Time = data.(field2).Time + options.TestTurbineTimeShift;
+
+    % calculate the durations between all time steps for the pre-test
+    % period
+    time_diff_field1 = diff (data.(field1).Time);
+    time_diff_field2 = diff (data.(field2).Time);
+
+    % identify the data where the durations between time steps are below a
+    % chosen threshold, e.g. 5 x the nominal sample period or something
+    sample_threshold_field1 = time_diff_field1 < options.DataGapInfillThreshold;
+    sample_threshold_field2 = time_diff_field2 < options.DataGapInfillThreshold;
+
+    % find the transistions between 0-1 and 1-0 to locate where in the time
+    % series the 'good' (without a gap in the time steps) blocks start and
+    % end
+    field1_good_start_time_inds = find(diff([false, sample_threshold_field1]) == 1);
+    field1_good_end_time_inds = find(diff([sample_threshold_field1, false]) == -1) + 1;
+
+    field2_good_start_time_inds = find(diff([false, sample_threshold_field2]) == 1);
+    field2_good_end_time_inds = find(diff([sample_threshold_field2, false]) == -1) + 1;
+
+    % create a new time series with the desired time step, starting from
+    % the earlier of the two time series and ending at the later
+    new_series_start_time = round2 (min ( [data.(field1).Time(:); data.(field2).Time(:)] ), options.SyncronisedTimeStepSize);
+    new_series_end_time = round2 (max ( [data.(field1).Time(:); data.(field2).Time(:)] ), options.SyncronisedTimeStepSize);
+
+    new_time = new_series_start_time : options.SyncronisedTimeStepSize : new_series_end_time;
+
+    % find the overlapping times
+    field1_good_new_times = false(size (new_time));
+    for ind = 1:numel(field1_good_start_time_inds)
+
+        field1_good_new_times = field1_good_new_times | ...
+                                    ( new_time >= data.(field1).Time (field1_good_start_time_inds(ind)) ...
+                                        & new_time <= data.(field1).Time (field1_good_end_time_inds(ind)) );
+
+    end
+
+    field2_good_new_times = false(size (new_time));
+    for ind = 1:numel(field2_good_start_time_inds)
+
+        field2_good_new_times = field2_good_new_times | ...
+                                    ( new_time >= data.(field2).Time (field2_good_start_time_inds(ind)) ...
+                                        & new_time <= data.(field2).Time (field2_good_end_time_inds(ind)) );
+
+    end
+
+    % the new times at which we want the interpolated values from both
+    % time series
+    new_time = new_time(field1_good_new_times & field2_good_new_times);
+
+    % now interpolate all data fields at the desired new time points which
+    % are common to both time series
+    data_fieldnames = fieldnames (data.(field1));
+
+    for data_field_name = data_fieldnames'
+
+        if ~strcmp(data_field_name{1}, 'Time')
+            data.(field1).(data_field_name{1}) = interp1 (data.(field1).Time, data.(field1).(data_field_name{1}), new_time);
+            data.(field2).(data_field_name{1}) = interp1 (data.(field2).Time, data.(field2).(data_field_name{1}), new_time);
+        end
+
+    end
+
+    data.(field1).Time = new_time;
+    data.(field2).Time = new_time;
+
+end
+
+function turbine_data = leave_data_good_inds_all_fields (turbine_data, good_inds)
 % strips data not reported as good from all fields of a structure
 
     data_fieldnames = fieldnames (turbine_data);
 
     for data_field_name = data_fieldnames'
 
-        turbine_data.(data_field_name) = turbine_data.(data_field_name)(good_inds);
+        turbine_data.(data_field_name{1}) = turbine_data.(data_field_name{1})(good_inds);
 
     end
 
 end
 
-function data = strip_data_bad_inds_all_fields (turbine_data, bad_inds)
+function turbine_data = strip_data_bad_inds_all_fields (turbine_data, bad_inds)
 % strips data reported as bad from all fields of a structure
 
     data_fieldnames = fieldnames (turbine_data);
 
     for data_field_name = data_fieldnames'
 
-        turbine_data.(data_field_name)(bad_inds) = [];
+        turbine_data.(data_field_name{1})(bad_inds) = [];
 
     end
 
---
+++ test/test_windp2p_filterData.m
@@ -0,0 +1,64 @@
+% test_windp2p_filterData
+
+%% Generate dummy pre data for testing
+data.ControlWTPre.Time = 0:1:400;
+data.ControlWTPre.Time = data.ControlWTPre.Time + (rand(size(data.ControlWTPre.Time)) - 0.5) .* 0.1;
+
+% create some gaps in the data
+data.ControlWTPre.Time(data.ControlWTPre.Time >= 10 & data.ControlWTPre.Time <= 20) = [];
+data.ControlWTPre.Time(data.ControlWTPre.Time >= 50 & data.ControlWTPre.Time <= 70) = [];
+data.ControlWTPre.Time(data.ControlWTPre.Time >= 160 & data.ControlWTPre.Time <= 180) = [];
+
+data.ControlWTPre.RPM = 8 .* sin (period2omega(60) .* data.ControlWTPre.Time) + 8;
+data.ControlWTPre.PowerActive = 1e6 .* data.ControlWTPre.RPM;
+data.ControlWTPre.TemperatureExternal = zeros (size (data.ControlWTPre.Time)) + 10;
+data.ControlWTPre.DirectionNacelle = zeros (size (data.ControlWTPre.Time));
+
+data.TestWTPre.Time = 3:1:500;
+data.TestWTPre.Time = data.TestWTPre.Time + (rand(size(data.TestWTPre.Time)) -  0.5) .* 0.1;
+
+% create some gaps in the data
+data.TestWTPre.Time(data.TestWTPre.Time >= 15 & data.TestWTPre.Time <= 25) = [];
+data.TestWTPre.Time(data.TestWTPre.Time >= 80 & data.TestWTPre.Time <= 90) = [];
+data.TestWTPre.Time(data.TestWTPre.Time >= 150 & data.TestWTPre.Time <= 170) = [];
+
+data.TestWTPre.RPM = 0.97 .* 8 .* sin (period2omega(60) .* data.TestWTPre.Time) + 8;
+data.TestWTPre.PowerActive = 1.03 .* 1e6 .* data.TestWTPre.RPM;
+data.TestWTPre.TemperatureExternal = zeros (size (data.TestWTPre.Time)) + 10;
+data.TestWTPre.DirectionNacelle = zeros (size (data.TestWTPre.Time));
+
+%% generate dummy post data for testing
+data.ControlWTPost.Time = data.ControlWTPre.Time + 10000;
+data.ControlWTPost.Time = data.ControlWTPost.Time + (rand(size(data.ControlWTPost.Time)) - 0.5) .* 0.1;
+
+% data.ControlWTPost.Time(data.ControlWTPost.Time >= 10 & data.ControlWTPost.Time <= 20) = [];
+% data.ControlWTPost.Time(data.ControlWTPost.Time >= 50 & data.ControlWTPost.Time <= 70) = [];
+% data.ControlWTPost.Time(data.ControlWTPost.Time >= 160 & data.ControlWTPost.Time <= 180) = [];
+
+data.ControlWTPost.RPM = 8 .* sin (period2omega(30) .* data.ControlWTPost.Time) + 8;
+data.ControlWTPost.PowerActive = 1e6 .* data.ControlWTPost.RPM;
+data.ControlWTPost.TemperatureExternal = zeros (size (data.ControlWTPost.Time)) + 10;
+data.ControlWTPost.DirectionNacelle = zeros (size (data.ControlWTPost.Time));
+
+data.TestWTPost.Time = data.TestWTPre.Time + 10000;
+data.TestWTPost.Time = data.TestWTPost.Time + rand(size(data.TestWTPost.Time))*0.1;
+
+% data.TestWTPost.Time(data.TestWTPost.Time >= 15 & data.TestWTPost.Time <= 25) = [];
+% data.TestWTPost.Time(data.TestWTPost.Time >= 80 & data.TestWTPost.Time <= 90) = [];
+% data.TestWTPost.Time(data.TestWTPost.Time >= 150 & data.TestWTPost.Time <= 170) = [];
+% data.TestWTPost.Time
+
+data.TestWTPost.RPM = 0.97 .* 8 .* sin (period2omega(30) .* data.TestWTPost.Time) + 8;
+data.TestWTPost.PowerActive = 1.03 .* 1e6 .* data.TestWTPost.RPM;
+data.TestWTPost.TemperatureExternal = zeros (size (data.TestWTPost.Time)) + 10;
+data.TestWTPost.DirectionNacelle = zeros (size (data.TestWTPost.Time));
+
+%% 
+
+filtered_data = windp2p.filterData (data, 'WindDirectionNumSamplesThresh', 20);
+
+scatter ( data.ControlWTPre.Time, data.ControlWTPre.RPM, ...
+          filtered_data.ControlWTPre.Time, filtered_data.ControlWTPre.RPM);
+
+scatter ( data.TestWTPre.Time, data.TestWTPre.RPM, ...
+          filtered_data.TestWTPre.Time, filtered_data.TestWTPre.RPM);
